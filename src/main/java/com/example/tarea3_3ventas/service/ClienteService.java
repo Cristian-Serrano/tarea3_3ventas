e7d5e256e69883af696f3d9a40cd9b40b032a4a7
Cristian-Serrano <gcristiag@gmail.com>
2024-01-12 10:47:27 +0100

    a

--- src/main/java/com/example/tarea3_3ventas/service/ClienteService.java
+++ src/main/java/com/example/tarea3_3ventas/service/ClienteService.java
@@ -1,12 +1,46 @@
-package com.example.tarea3_3ventas.service;
+import java.util.List;
+import java.util.Optional;
 
-import com.example.tarea3_3ventas.dao.ClienteDAO;
+import com.example.tarea3_3ventas.domain.Cliente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ClienteService {
+public class ClienteServicio {
 
     @Autowired
-    private ClienteDAO clienteDAO;
+    private FabricanteDAO fabricanteDAO;
+
+    public List<Cliente> listAll() {
+
+        return fabricanteDAO.getAll();
+
+    }
+
+    public Cliente one(Integer id) {
+        Optional<Cliente> optFab = fabricanteDAO.find(id);
+        if (optFab.isPresent())
+            return optFab.get();
+        else
+            return null;
+    }
+
+    public void newFabricante(Cliente cliente) {
+
+        fabricanteDAO.create(cliente);
+
+    }
+
+    public void replaceFabricante(Cliente cliente) {
+
+        fabricanteDAO.update(cliente);
+
+    }
+
+    public void deleteFabricante(int id) {
+
+        fabricanteDAO.delete(id);
+
+    }
+
 }
