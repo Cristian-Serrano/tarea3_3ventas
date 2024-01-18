c9bea3fd33a7fe904259133e47b28b2eba8b1227
cristian <ejemplo@ejemplo.com>
2024-01-18 19:24:33 +0100

    18 enero

--- src/main/java/com/example/tarea3_3ventas/controller/ClienteController.java
+++ src/main/java/com/example/tarea3_3ventas/controller/ClienteController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 
@@ -24,6 +27,23 @@ public class ClienteController {
         return "clientes";
     }
 
+    @GetMapping({"clientes/crear","clients/create"})
+    public String crear(Model model){
+
+        Cliente cliente = new Cliente();
+        model.addAttribute("cliente", cliente);
+
+        return "crearCliente";
+    }
+
+    @PostMapping({"clientes/crear","clients/create"})
+    public RedirectView submitCrear(Model model, Cliente cliente){
+
+        clienteService.create(cliente);
+
+        return new RedirectView("/clientes");
+    }
+
     @GetMapping({"/clientes/{id}","/clients/{id}"})
     public String detalle(Model model, @PathVariable Integer id ){
         model.addAttribute("cliente", clienteService.one(id));
@@ -31,10 +51,26 @@ public class ClienteController {
         return "detalleCliente";
     }
 
-    @GetMapping({"clientes/cambiar/{id}","clients/update/{id}"})
-    public String cambiar(Model model, @PathVariable Integer id ){
+    @GetMapping({"clientes/editar/{id}","clients/update/{id}"})
+    public String editar(Model model, @PathVariable Integer id ){
         model.addAttribute("cliente", clienteService.one(id));
 
         return "editarCliente";
+    }
+
+    @PostMapping({"/clientes/editar/{id}","/clients/update/{id}"})
+    public RedirectView submitEditar(@ModelAttribute("cliente") Cliente cliente) {
+
+        clienteService.replace(cliente);
+
+        return new RedirectView("/clientes");
+    }
+
+    @PostMapping({"/clientes/borrar/{id}","/clients/delete/{id}"})
+    public RedirectView borrar(@PathVariable Integer id) {
+
+        clienteService.delete(id);
+
+        return new RedirectView("/clientes");
     }
 }
--- src/main/java/com/example/tarea3_3ventas/domain/Comercial.java
+++ src/main/java/com/example/tarea3_3ventas/domain/Comercial.java
@@ -2,9 +2,11 @@ package com.example.tarea3_3ventas.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Comercial {
     private int id;
     private String nombre;
